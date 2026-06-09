56cbb57c6e5ec0786d4454313e2a236f0a782a68
Renn <its.happiness.2005@gmail.com>
2026-06-09 11:30:05 +0300

    buttons connected and coroutine is there

---
+++ SortingAlgorithms/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void StartAlgorithm1()
+    {
+        SceneManager.LoadScene("Alg1");
+    }
+}
